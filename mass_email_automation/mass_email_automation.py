cc4a3b3b00f7a1dcf0d6488160d18f74cd4f74ae
ritviksehgal <50535512+ritviksehgal@users.noreply.github.com>
2023-03-26 13:47:17 -0400

    Update mass_email_automation.py

--- mass_email_automation/mass_email_automation.py
+++ mass_email_automation/mass_email_automation.py
@@ -7,7 +7,7 @@ file_loc = "{file_path}"
 read = pd.read_excel(file_loc, index_col=0, header =0)
 
 user_email = input("Enter your email: ")
-user_pass = input("Enter your password: ")
+user_pass = input("Enter application- specific password: ") # information on app- specific password found at https://support.google.com/accounts/answer/185833?visit_id=638154494702794850-185197231
 
 # Set up the SMTP server
 server = smtplib.SMTP('Smtp.gmail.com')
