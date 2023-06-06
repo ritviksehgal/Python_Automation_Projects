495d50058097643d616ce54b96ca1fc70b67adea
ritviksehgal <50535512+ritviksehgal@users.noreply.github.com>
2023-06-06 13:50:58 -0400

    Update Input_data_using_VirusTotal_API_Output_Secure_Score.py

--- VirusTotal/Input_data_using_VirusTotal_API_Output_Secure_Score.py
+++ VirusTotal/Input_data_using_VirusTotal_API_Output_Secure_Score.py
@@ -1,17 +1,20 @@
 import json
 import requests
-import openpyxl
-import xlsxwriter
 import time
 import pandas as pd
-import numpy as np
+from datetime import datetime
 
 
-file_loc = "input_file_location_here"
+current_datetime = datetime.now()
+formatted_datetime = current_datetime.strftime("%Y-%m-%d")
+
+file_loc = f"<file_path>"
 df = pd.read_excel(file_loc, index_col=None, usecols= 'A, B', header = None)
 df = df.head(220)
 
-api_key = "input_API_key"
+
+
+api_key = "<API_key>"
 base_url = "https://www.virustotal.com/api/v3/files/"
 headers = {"accept": "application/json", 'X-Apikey': api_key}
 
@@ -26,14 +29,14 @@ for MD5_hash in df[1].to_list():
 
             if response_json['data']['attributes']['last_analysis_stats']['malicious'] >=2:
                 with open ('vt_results.txt', 'a') as vt:
-                    vt.write(url) and vt.write('-\tMalicious\n')
+                    vt.write(MD5_hash) and vt.write('-\tMalicious\n')
 
 
             if response_json['data']['attributes']['last_analysis_stats']['malicious'] <=1:
                 with open ('vt_results.txt', 'a') as vt:
-                    vt.write(url) and vt.write(' -\tNot Malicious\n')
+                    vt.write(MD5_hash) and vt.write(' -\tNot Malicious\n')
         except:
             with open ('vt_results.txt', 'a') as vt:
-               vt.write(url) and vt.write('-\tSecure score not found\n')
+               vt.write(MD5_hash) and vt.write('-\tSecure score not found\n')
             time.sleep(20)
             continue
