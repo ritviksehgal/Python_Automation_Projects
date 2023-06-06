8ce144c35224d81056267c84716e0e21b9a05aad
ritviksehgal <50535512+ritviksehgal@users.noreply.github.com>
2023-06-06 13:52:35 -0400

    Update mass_email_automation.py

--- mass_email_automation/mass_email_automation.py
+++ mass_email_automation/mass_email_automation.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 # Read the data from the Excel file
 df = pd.read_excel('emails_names_laptopIDs.xlsx')
-file_loc = "C:/Users/ritvi/OneDrive/Desktop/py_scripts/mass_email_automation/emails_names_laptopIDs.xlsx"
-read = pd.read_excel(file_loc, index_col=0, header =0)
+file_loc = "C:/Users/ritvi/OneDrive/Documents/Desktop/py_scripts/mass_email_automation/emails_names_laptopIDs.xlsx"
+read = pd.read_excel(file_loc, header =0)
 
 user_email = input("Enter your email: ")
 user_pass = input("Enter your application- specific password: ")  # create an app- specific password at https://myaccount.google.com/apppasswords
@@ -20,15 +20,12 @@ for i, row in df.iterrows():
     to_email = row['Email']
     name = row['Name']
     laptop_id = row['Laptop ID']
-    cc_email = input('Input the "CC" email here (optional):')
+    #cc_email = input('Input the "CC" email here (optional):')
 
     # Compose the email message
     subject = 'Your Laptop is Ready for Pickup!'
     message =f"Subject: {subject}\n\n Dear {name},\n\nYour laptop {laptop_id} is ready for pickup. Please come to the IT Service Center to claim it.\n\nBest regards,\nService Desk- Global".format(name, laptop_id)
-    
-
-from_email = input('Input the "From" email here:')
-    # Send the email
-server.sendmail(from_email, [to_email, cc_email], message)
+    from_email = user_email
+    server.sendmail(from_email, to_email, message)
 
 server.quit()
